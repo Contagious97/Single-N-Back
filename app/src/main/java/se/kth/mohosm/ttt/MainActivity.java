d02b6e5099c0dd0b3a67f17f73da2f765ffffc7d
Roibin <jamsahar@kth.se>
2021-12-02 17:01:59 +0100

    landscape view has some problems

--- app/src/main/java/se/kth/mohosm/ttt/MainActivity.java
+++ app/src/main/java/se/kth/mohosm/ttt/MainActivity.java
@@ -8,27 +8,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.PreferenceManager;
-import androidx.preference.SeekBarPreference;
 
+import se.kth.mohosm.ttt.model.GameSettings;
 import se.kth.mohosm.ttt.model.TicLogic;
 import se.kth.mohosm.ttt.utils.AnimationUtils;
 import se.kth.mohosm.ttt.utils.TextToSpeechUtil;
@@ -46,12 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TextToSpeechUtil textToSpeechUtil;
 
-    private TextView currentEventsCounter;
-    private TextView valueOfN;
-    private TextView selectedStimuli;
-    private TextView timeBetweenEvents;
+    private TextView currentEventsCounterTV;
+    private TextView valueOfNTV;
+    private TextView selectedStimuliTV;
+    private TextView timeBetweenEventsTV;
 
-    @SuppressLint("CutPasteId")
+    @SuppressLint({"CutPasteId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,42 +64,118 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         String dropdown = preferences.getString("dropdown", "");
 
-        currentEventsCounter = (TextView) findViewById(R.id.curr_nr_events_text);
-        valueOfN = (TextView) findViewById(R.id.time_events_text);
-        selectedStimuli = (TextView) findViewById(R.id.sel_stimuli_text);
-        timeBetweenEvents = (TextView) findViewById(R.id.time_events_text);
+        currentEventsCounterTV = (TextView) findViewById(R.id.curr_nr_events_text);
+        valueOfNTV = (TextView) findViewById(R.id.value_of_n);
+        selectedStimuliTV = (TextView) findViewById(R.id.sel_stimuli_text);
+        timeBetweenEventsTV = (TextView) findViewById(R.id.time_events_text);
 
+        //default and initial gameSettings
+        GameSettings.setAudioStimuli(false);
+        GameSettings.setPatternStimuli(false);
+        GameSettings.setNrOfEvents(30);
+        GameSettings.setTimeBetweenEvents(2500);
+        GameSettings.setValueOfN(1);
+
+        currentEventsCounterTV.setText("Current number of events: " + "0 of " + GameSettings.getNrOfEvents());
+        valueOfNTV.setText("Value of n: " + GameSettings.getValueOfN());
+        selectedStimuliTV.setText("Selected: No stimuli selected");
+        timeBetweenEventsTV.setText("Time between events: " + GameSettings.getTimeBetweenEvents() + "ms");
+
+        //checks whether phone is flipped or not
+        isFlipped(savedInstanceState);
+
+
+//        if (GameSettings.isAudioStimuli()){
+//            selectedStimuli.setText("Selected: Audio stimuli");
+//        }
+//        if(GameSettings.isPatternStimuli()){
+//            selectedStimuli.setText("Selected: Pattern stimuli");
+//        }
+//        else{
+//            selectedStimuli.setText("Selected: No stimuli selected");
+//        }
         updateImageViews(null); // game might already be started, so update image views
     }
 
-    public void launchSettingsActivity(View view) {
-        Log.d(LOG_TAG, "Settings in list clicked!");
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
+    /**
+     * Saves the states, meaning saving the data so that it can be used in portrait/land mode
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // get data from textViews
+        String valueOfN = valueOfNTV.getText().toString();
+        String currentEventsCounter = currentEventsCounterTV.getText().toString();
+        String timeBetweenEvents = timeBetweenEventsTV.getText().toString();
+        String selectedStimuli = selectedStimuliTV.getText().toString();
+
+        // tests so we can see that the right data is being saved in the later stage
+        Log.d(LOG_TAG, valueOfN);
+        Log.d(LOG_TAG, currentEventsCounter);
+        Log.d(LOG_TAG, timeBetweenEvents);
+        Log.d(LOG_TAG, selectedStimuli);
+
+        savedInstanceState.putString("value_of_n", valueOfN);
+        savedInstanceState.putString("curr_nr_events_text", currentEventsCounter);
+        savedInstanceState.putString("time_events_text", timeBetweenEvents);
+        savedInstanceState.putString("sel_stimuli_text", selectedStimuli);
     }
+
+
+    /**
+     * Checks if the phone has flipped from portrait to horizon and vice versa
+     * If indeed flipped save the current state so that it does not perish when the application stops, creates and starts
+     *
+     * @param savedInstanceState
+     */
+    public void isFlipped(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+
+            // GameSettings
+
+            String savedValueOfN = savedInstanceState.getString("value_of_n");
+            String savedCurrentEventsCounter = savedInstanceState.getString("curr_nr_events_text");
+            String savedSelectedStimuli = savedInstanceState.getString("time_events_text");
+            String savedTimeBetweenEvents = savedInstanceState.getString("sel_stimuli_text");
+            // tests so we can see that the right data is being set in the textViews
+            Log.d(LOG_TAG, savedValueOfN);
+            Log.d(LOG_TAG, savedCurrentEventsCounter);
+            Log.d(LOG_TAG, savedSelectedStimuli);
+            Log.d(LOG_TAG, savedTimeBetweenEvents);
+
+            currentEventsCounterTV.setText(savedCurrentEventsCounter);
+            valueOfNTV.setText(savedValueOfN);
+            selectedStimuliTV.setText(savedSelectedStimuli);
+            timeBetweenEventsTV.setText(savedTimeBetweenEvents);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.settings_view,menu);
+        menuInflater.inflate(R.menu.settings_view, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        Log.d(LOG_TAG,"Inside the onOptionsIntemSelected");
-        switch (item.getItemId()){
+        Log.d(LOG_TAG, "Inside the onOptionsIntemSelected");
+        switch (item.getItemId()) {
             case R.id.settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
-            default:return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         super.onBackPressed();
         finish();
     }
@@ -151,7 +221,7 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         msg = "A draw, try again";
                 }
-                UiUtils.createDialog(this,"Game ower", msg).show();
+                UiUtils.createDialog(this, "Game ower", msg).show();
                 textToSpeechUtil.speakNow(msg);
             }
         }
@@ -183,7 +253,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 int index = r * SIZE + c;
                 imageViews[index].setImageDrawable(img); // index in array imageViews
-                if(imageViews[index]== tappedView) {
+                if (imageViews[index] == tappedView) {
                     AnimationUtils.fadeInImageView(tappedView);
                 }
             }
@@ -216,6 +286,6 @@ public class MainActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         // to prevent the staus bar from reappearing in landscape mode when,
         // for example, a dialog is shown
-        if(hasFocus) UiUtils.setStatusBarHiddenInLandscapeMode(this);
+        if (hasFocus) UiUtils.setStatusBarHiddenInLandscapeMode(this);
     }
 }
